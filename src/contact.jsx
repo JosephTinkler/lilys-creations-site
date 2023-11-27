f571e8d0906af26ddd9dfd7fae0ae3ecc163195f
Joe Tinkler <joe@josephtinkler.com>
2023-11-27 17:34:19 +0000

    update contact info page and main description

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,6 +5,9 @@ import lilyPhoto from '/images/lily_logo.png';
 import frogNoBg from '/images/Lily-frog-nobg.png';
 import frogMain from '/images/Lily-frog-main.png';
 import mainimg from '/images/mainimg.png';
+import instagram from '/images/instagram.svg';
+import mail from '/images/mail.svg';
+import linkedin from '/images/linkedin.svg';
 
 
 const Contact = () => {
@@ -12,9 +15,17 @@ const Contact = () => {
     return (
         <div className="mb-32">
             <Header />
-            
-            <div id="contact-div" className="">
-               
+            <div id="contact-about" className="flex justify-center align-center"></div>
+            <div id="contact-div" className="flex gap-2 justify-center align-center mt-32">
+               <a className="flex justify-center align-center" target="_blank" href="https://www.instagram.com/lily.g.beer/">
+                <img src={instagram} />
+               </a>
+               <a className="flex justify-center align-center" target="_blank" href="mailto:lilygbeer@gmail.com">
+                    <img src={mail} />
+                </a>
+                <a className="flex justify-center align-center" target="_blank" href="https://uk.linkedin.com/in/lily-beer-2b530033a">
+                    <img src={linkedin} />
+                </a>
             </div>  
         </div>
     );
--- src/home.jsx
+++ src/home.jsx
@@ -20,15 +20,16 @@ const Home = () => {
             <div className="flex flex-col items-center justify-center mt-32 mb-64">
                 <img src={frogNoBg} className="bounce-animation image-shadow" alt="Lily Photo" style={{ width: "150px", height: "auto" }}/>
                 <h1 className="mt-3">Lily's Modelmaking</h1>
-                <button onClick={scrollToSection} className="button mt-16">Read More</button>
+                <button onClick={scrollToSection} className="button mt-16 mb-16">Read More</button>
             </div>
             <div id="about-div" className="mx-12 mt-5 flex">
                 <div id="about-text" className="w-1/2">
                     <h1 className='mb-2'>Hello, my name is Lily Beer</h1>
                     <h2 className='text-black mb-4'>I am currently studying BA(Hons) Modelmaking at Arts University Bournemouth.</h2>
-                    <p className='text-black'>Lorem ipsum odor amet, consectetuer adipiscing elit. Amet faucibus dapibus est eleifend senectus euismod. Ad ridiculus natoque laoreet aliquam erat mollis. Curabitur nam hac enim sollicitudin proin nibh nibh. Class semper felis eleifend ornare pulvinar eleifend fringilla. Orci nascetur fermentum tortor efficitur rutrum.
+                    <p className='text-black pr-2'>I am a passionate maker dedicated to craftsmanship, sustainability, and engaging design. My work focuses on fostering connections—whether through nature, education, or art. I studied BA (Hons) Modelmaking at Arts University Bournemouth, where I developed my skills in creating high-quality, detailed models for educational and interactive experiences.
 
-Sollicitudin ultrices eget libero sagittis orci dictumst mattis. Sollicitudin fringilla urna sapien ultricies at bibendum diam nascetur. Varius erat enim eros malesuada ipsum vivamus, etiam parturient. Per metus maximus at ad magnis diam egestas inceptos. Class gravida suspendisse vehicula pretium praesent lacus libero varius sit. Posuere convallis sollicitudin integer montes cras felis. Felis primis sociosqu nulla purus cursus? Tincidunt ultrices lectus magnis himenaeos diam vestibulum senectus lacinia. Malesuada dignissim facilisis porta bibendum quam faucibus tortor luctus.</p>
+My values centre around fostering a connection to nature and art. I have always had a passion for animals and have pursued this interest through studies in Animal Management and Dog Grooming at Merrist Wood College. I am particularly drawn to creating meaningful, interactive experiences that inspire and educate others. I have created interactive, educational models for organizations like Dorset Heath Partnership and Blackpool Zoo, to inspire learning and engagement.
+I value traditional craftsmanship and embrace how artisanal processes create lasting connections with objects. Objects crafted with skill and care fulfill our innate desire for unique, human-centered design, in contrast to mass-produced, low-quality items. I embrace this by upcycling old items, shopping at charity shops, and challenging myself to create rather than buy whenever possible. I value the ‘reduce, reuse, recycle’ approach, which not only benefits the environment but also saves money.</p>
                 </div>
                 <div id="image-div" className="w-1/2">
                     <img src={mainimg} alt="Lily's Creations Logo" className="w-full h-full object-cover rounded-3xl" />
