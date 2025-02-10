c2336e20b1a03ad6f3396be188d029a55d8ec629
Joe Tinkler <joe@josephtinkler.com>
2025-02-10 22:26:09 +0000

    fix path issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./home";
 import Portfolio from "./portfolio"
+import Contact from "./contact"
 
 const App = () => {
     return (
